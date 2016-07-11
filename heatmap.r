6b397729e73a840ee3c5062521cde3ad7468744a
cimendes <iines.mendes@gmail.com>
2016-07-11 16:38:44 +0100

    -cleaned script - printing clustergram for all genes and 4000 genes with highest variance

--- heatmap.r
+++ heatmap.r
@@ -1,3 +1,4 @@
+# Installing Dependencies
 if (!require("gplots")){
   install.packages("gplots",dependencies = TRUE)
   library(gplots)
@@ -11,67 +12,79 @@ if(!require("Heatplus")){
   biocLite("Heatplus")  # annHeatmap or annHeatmap2
   library(Heatplus)
 }
-if(!require("vegan")){
-  install.packages("vegan", dependencies=TRUE)
-  library(vegan)
-}
-if (!require("ape")){
-  install.packages("ape",dependencies = TRUE)
-  library(ape)
-}
-
 
 # MAPPING
+#read gene_presence_absence.Rtab file
+file_gene = file.choose()
+filename_gene = basename(file_gene)
 
-# Unbiased
-rpkm <- read.table("gene_presence_absence.Rtab", sep="\t", header = TRUE)
-rownames(rpkm) <- rpkm[, 1]
-colnames(rpkm) <- gsub('_02062016', "",colnames(rpkm),fixed=TRUE)
-rpkm <- rpkm[, -1]
+gene_data <- read.table(filename_gene, sep="\t", header = TRUE)
+rownames(gene_data) <- gene_data[, 1]
+colnames(gene_data) <- gsub('_02062016', "",colnames(gene_data),fixed=TRUE)
+gene_data <- gene_data[, -1]
 
-#grouping
-groups <- read.table("grouping.txt", sep="\t", header = FALSE)
+# GROUPPING
+#read grouping.txt file
+file_groups = file.choose()
+filename_groups = basename(file_groups)
+
+groups <- read.table(filename_groups, sep="\t", header = FALSE)
 groups<- groups[,c(-4,-5)]
 groups[,3]<-gsub('forstgreen','forestgreen', groups[,3],fixed = TRUE)
-groups <- groups[match(colnames(rpkm), as.character(groups[,1])) ,]
-stopifnot(all(as.character(groups[,1]) == colnames(rpkm)))
-#rpkm<-cbind(rpkm,groups[,3])
+groups <- groups[match(colnames(gene_data), as.character(groups[,1])) ,]
+stopifnot(all(as.character(groups[,1]) == colnames(gene_data)))
 
-# Variance
-variance_rnaSeq <- apply(rpkm, 1, var)
+# Heatmap colour pallete
+my_palette <- colorRampPalette(c("white", "blue", "darkblue"))(n = 299)
+
+
+##### 4000 GENES WITH HIGHER VARIANCE #####
+
+# variance - select 4000 genes with higher variance
+variance_rnaSeq <- apply(gene_data, 1, var)
 variance_rnaSeq <- sort(variance_rnaSeq, decreasing = TRUE)
 variance_rnaSeq <- variance_rnaSeq[1:4000]
 
-#Dendogram from Mega
-#cPhylo<-read.tree(file="ML_Bt500.nwk")
-#dg=chronos(cPhylo)
-
-# Heatmap
-colour <- colorRampPalette(c("blue","white"),space="rgb")(75)
-my_palette <- colorRampPalette(c("white", "blue", "darkblue"))(n = 299)
-scaleyellowred <- colorRampPalette(c("red",'pink', "lightyellow"), space = "rgb")(100)
-
 #Fixing col names
-data=as.matrix(rpkm[match(names(variance_rnaSeq), rownames(rpkm)),])
-col_name=colnames(data)
-#col_name=gsub('_02062016', "",col_name,fixed=TRUE)
-row_name=rownames(data)
-
-
-#dendogram-vegan - NOT WORTH!
-#calculate the Bray-Curtis dissimilarity matrix on the full dataset:
-#data.dist <- vegdist(data, method = "bray")
-# Do average linkage hierarchical clustering. Other options are 'average', 'complete' or 'single'. You'll need to choose the one that best fits the needs of your situation and your data.
-#row.clus <- hclust(data.dist, "average")
+data_variance=as.matrix(gene_data[match(names(variance_rnaSeq), rownames(gene_data)),])
+col_name=colnames(data_variance)
+row_name=rownames(data_variance)
 
 # creates a 5 x 5 inch image
-png("cluster_4000_blue.png",        
+png("clustergram_4000variable.png",        
     width = 13*300,        # 5 x 300 pixels
     height = 9*300,
     res = 500,            # 300 pixels per inch
     pointsize = 8)        # smaller font size
 
-heatmap.2(data, 
+heatmap.2(data_variance, 
+          scale='none',
+          notecol="black", 
+          labCol=col_name,
+          density.info="none",  
+          trace="none",         
+          margins =c(12,9),
+          ColSideColors = as.character(groups[,3]),
+          col=my_palette,
+          labRow='', 
+          dendrogram="both")
+dev.off()
+
+##### ALL DATA #####
+
+#Fixing col names
+data_all=as.matrix(gene_data)
+col_name=colnames(data_all)
+row_name=rownames(data_all)
+
+# creates a 5 x 5 inch image
+png("clustergram_allData.png",        
+    width = 13*300,        # 5 x 300 pixels
+    height = 9*300,
+    res = 500,            # 300 pixels per inch
+    pointsize = 8)        # smaller font size
+
+heatmap.2(data_all, 
           scale='none',
           notecol="black", 
           labCol=col_name,
